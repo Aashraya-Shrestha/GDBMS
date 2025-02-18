2a4d8cd3cfdbc24b3fec89a15528c7720822a62b
Aashraya-Shrestha <aashraya8@gmail.com>
2025-02-18 22:17:11 +0545

    sidebar logic set

--- gdbms-frontend/src/App.js
+++ gdbms-frontend/src/App.js
@@ -2,15 +2,33 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
+import Dashboard from "./Pages/Dashboard";
+import SideBar from "./Components/sidebar";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    let isLoggedIn = sessionStorage.getItem("isLogin");
+    if (isLoggedIn) {
+      setIsLogin(true);
+      navigate("/dashboard");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sessionStorage.getItem("isLogin")]);
   return (
-    <Router>
+    <div className="flex">
+      {isLogin && <SideBar />}
+
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 

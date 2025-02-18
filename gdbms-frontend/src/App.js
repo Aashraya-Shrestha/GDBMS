579a5b66ca476fef1c77d6933a84bb9943515797
Aashraya-Shrestha <aashraya8@gmail.com>
2025-02-18 22:19:34 +0545

    Updated imports

--- gdbms-frontend/src/App.js
+++ gdbms-frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
 import Dashboard from "./Pages/Dashboard";

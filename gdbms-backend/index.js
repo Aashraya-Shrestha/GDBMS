2ef3a75291c602f6280ad88eeac959869f30c97c
Aashraya-Shrestha <aashraya8@gmail.com>
2025-02-24 16:09:17 +0545

    installed dependencies and imported them

--- gdbms-backend/index.js
+++ gdbms-backend/index.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
+app.use(cookieParser());
+app.use(express.json());
 require("./DbConn/conn");
+
+const gymRoutes = require("./Routes/gymRoute");
+app.use("/auth", gymRoutes);
 
 app.get("/", (req, res) => {
   res.send({ message: "Your server is running" });

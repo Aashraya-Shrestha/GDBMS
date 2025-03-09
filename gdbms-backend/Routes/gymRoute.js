2f2fe8d745c709816b54f59a1a3e7e4154548396
Aashraya-Shrestha <aashraya8@gmail.com>
2025-03-09 08:29:58 +0545

    added route for the contact

--- gdbms-backend/Routes/gymRoute.js
+++ gdbms-backend/Routes/gymRoute.js
@@ -10,6 +10,7 @@ router.post("/reset-password/sendOTP", gymController.sendOTP);
 router.post("/reset-password/checkOTP", gymController.checkOTP);
 router.post("/reset-password", gymController.resetPassword);
 router.post("/logout", gymController.logout);
+router.post("/contact", gymController.sendContactMail);
 
 // Protected Route (requires authentication)
 router.get("/gymInfo", auth, gymController.getLoggedInGym); // Add this route
